a458db312a7c179e4cf3d7669900943c6d14c91e
CarAlvRag <99685553+CarAlvRag@users.noreply.github.com>
2022-04-27 20:03:40 -0500

    Update FiltroSMA.cpp

--- P01_EMA_y_SMA/FiltroSMA.cpp
+++ P01_EMA_y_SMA/FiltroSMA.cpp
@@ -105,23 +105,23 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	HAL_ADC_Start(&hadc1);								//Inicializa el ADC
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 1);			//Apaga el LED
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw = HAL_ADC_GetValue(&hadc1);						//Recepción de señal por ADC
-	voltaje = ((float)raw)/4095 * 3.3;					//Cambio a señal digital para resolución de 12 bits
+	HAL_ADC_Start(&hadc1);							// Inicializa el ADC
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 1);				// Apaga el LED
+	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);			// Realiza la conversión
+	raw = HAL_ADC_GetValue(&hadc1);						// Recepción de señal por ADC
+	voltaje = ((float)raw)/4095 * 3.3;					// Obtención de valor de voltaje para señal digital obtenida con resolución de 12 bits
 
 	y = (voltaje + suma) / cont; 						// Aplicación de Filtro SMA
-	suma += voltaje;									// a señal recibida
+	suma += voltaje;							// Acumulador de señal recibida
 
 
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 0);			//Prende el LED
-	sprintf(msg,"%.3f,",voltaje); 						//Separa una señal de otra
-	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, 0);				// Prende el LED
+	sprintf(msg,"%.3f,",voltaje); 						// Separa una señal de otra
+	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);  // Transmite por UART valor de voltaje obtenido
 	sprintf(msg,"%.3f\r\n",y);
-	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);	// Transmite por UART valor filtrado
 
-	HAL_Delay(500);
+	HAL_Delay(500);								// Retardo de 500 ms
 	cont++;
   }
   /* USER CODE END 3 */
